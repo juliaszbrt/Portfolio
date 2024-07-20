46c4b73feb61bd3fe8fc0e5fb1ad3d60a9d9734d
juliaszbrt <112975933+juliaszbrt@users.noreply.github.com>
2024-07-20 14:09:53 -0400

    Add animations + fix responsivity

--- src/app/About.jsx
+++ src/app/About.jsx
@@ -1,34 +1,43 @@
 import Image from "next/image"
 import "./globals.css"
+import "./app.js"
+import { motion } from "framer-motion";
 
 const About = () => {
   return (
     <div className="h-auto bg-[#F8C39A]">
-        <div className="flex flex-col gap-[3rem] lg:flex lg:flex-row lg:justify-center">
-            <div className="lg:flex gap-[4rem] lg:gap-[5rem] lg:my-[4rem] lg:items-center lg:basis-3/4 lg:justify-center">
-                <div className="flex flex-col items-center justify-center mt-[4rem] mb-[2rem] lg:my-0">
-                    <div className="relative h-[16rem] w-[16rem] lg:h-[18rem] lg:w-[18rem] rounded-full border-[1.5rem] border-[#FFDDC2] overflow-hidden">
+        <div className="lg:flex lg:flex-row lg:justify-center">
+            <div className="lg:flex lg:gap-[5rem] lg:my-[4rem] lg:items-center lg:basis-3/4 lg:mx-[4rem] lg:justify-center">
+                <motion.div
+                  initial={{ opacity: 0 }}
+                  whileInView={{ opacity: 1 }}
+                  viewport={{
+                      amount: "all",
+                      once: true,
+                  }}
+                  transition={{ duration: 1.5 }}
+                  className="flex items-center flex-col pt-[2rem] mb-[2rem] lg:my-0 lg:pl-[1rem]">
+                    <div className="relative h-[16rem] w-[16rem] lg:h-[18rem] lg:w-[18rem] 2xl:h-[20rem] 2xl:w-[20rem] rounded-full border-[1.5rem] border-[#FFDDC2] overflow-hidden">
                         <Image alt="profile photo" loading="lazy" width={1242} height={1242} src="/headshot.png" 
                         className="object-cover rounded-full"></Image>
                     </div>
-                    <div className="py-[2rem] text-[0.8rem] text-center tracking-[1.8px] leading-6 text-[#3C1D06] lg:text-[1rem] lg:leading-8 lg:pb-0">
+                    <div className="py-[2rem] text-[0.8rem] text-center tracking-[1.8px] leading-6 text-[#3C1D06] lg:text-[1rem] lg:leading-8 lg:pb-0 2xl:text-[1.3rem]">
                         Bachelor of Science (BS)
                         <br/>
                         <b>Computer Science, CO-OP</b>
                         <br/>
                         Toronto Metropolitan University
                     </div>
-                </div>
-                <div className="flex justify-center ">
-                    <div className="w-[20rem] lg:w-[22rem]">
-                        <h1 className="mb-[1rem] text-[1.7rem] tracking-[5px] font-semibold text-[#3C1D06] lg:text-[2rem]">
+                </motion.div>
+
+                <div className="flex justify-self-center">
+                    <div className="flex flex-col justify-center px-[3rem] mb-[2rem]">
+                        <h1 className="mb-[1rem] text-[1.7rem] tracking-[5px] font-semibold text-[#3C1D06] lg:text-[2rem] 2xl:text-[3rem]">
                             ABOUT ME...
                         </h1>
-                        <p className="text-justify text-[0.8rem] tracking-[1.8px] leading-6 text-[#3C1D06] lg:text-[1rem] lg:leading-8">
+                        <p className="text-justify text-[0.8rem] tracking-[1.8px] leading-6 text-[#3C1D06] lg:text-[1rem] lg:leading-8 2xl:w-[40rem] 2xl:text-[1.3rem] 2xl:leading-10">
                             I am <b>ambitious</b>; I put myself in situations where I can learn vital skills and gain new experiences.
                             I love to express my <b>passion</b> for the work that I do through unique designs and creative projects. 
-                            <br/>
-                            <br/>  
                         </p>
                     </div>
                 </div>
--- src/app/Home.jsx
+++ src/app/Home.jsx
@@ -1,6 +1,8 @@
 import Link from "next/link";
+import "./app.js"
 import Image from "next/image"
 import { Murecho } from 'next/font/google'
+import { motion } from "framer-motion";
 
 const murecho = Murecho({ subsets: ['latin'] })
 
@@ -9,16 +11,25 @@ const Home = () => {
     <div className="h-screen bg-[#FFECDD]">
       <div className="flex">
         <div className="h-screen bg-[#FFDDC2] lg:flex lg:basis-1/3">
-          {/* <Link href="./contact">
-            <button className="p-[3rem] absolute top-0 right-0 text-[20px] tracking-[2.5px] text-[#3C1D06]">
+          <Link href="./contact">
+            <button className="p-[3rem] absolute top-0 right-0 text-[20px] tracking-[2.5px] text-[#3C1D06] md:text-[30px]">
               Contact
             </button>
-          </Link> */}
+          </Link>
         </div>
         <div className="h-screen bg-[#FFECDD] lg:flex lg:basis-2/3">
           <div className="absolute  lg:ml-[2.5rem] whitespace-nowrap top-1/2 transform -translate-y-1/2 left-1/2 -translate-x-1/2 lg:left-[62%]">
-              <h1 className={`text-[#ffdf8f] text-center lg:text-left mt-[-2rem] text-[5rem] lg:text-[11rem] font-black ${murecho.className}`}>ジュリア</h1>
-              <h2 className="text-[#f8c137] text-center lg:text-left mt-[-0.5rem] lg:mt-[-2rem] text-[2rem] lg:text-[5rem] tracking-[5px] lg:tracking-[11px] font-bold">Julia Szubert</h2>
+              <motion.div
+                  initial={{ opacity: 0 }}
+                  whileInView={{ opacity: 1 }}
+                  viewport={{
+                      amount: "all",
+                      once: true,
+                  }}
+                  transition={{ duration: 1 }}>
+                <h1 className={`text-[#ffdf8f] text-center lg:text-left mt-[-2rem] text-[5rem] md:text-[11rem] 2xl:text-[16rem] font-black ${murecho.className}`}>ジュリア</h1>
+                <h2 className="text-[#f8c137] text-center lg:text-left mt-[-0.5rem] lg:mt-[-2rem] text-[2rem] md:text-[5rem] 2xl:text-[7rem] tracking-[5px] lg:tracking-[11px] font-bold">Julia Szubert</h2>
+              </motion.div>
           </div>
         </div>
       </div>
