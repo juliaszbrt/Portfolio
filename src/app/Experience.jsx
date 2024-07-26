4ba41f9bb13766a021cf0fd93f960263714e66a8
juliaszbrt <112975933+juliaszbrt@users.noreply.github.com>
2024-07-26 00:39:37 -0400

    Add circles

--- src/app/Experience.jsx
+++ src/app/Experience.jsx
@@ -1,27 +1,49 @@
 const Experience = () => {
   return (
-    <div className="h-auto px-[5rem] py-[10rem]">
-      <div className="flex justify-center">
-        <div className="flex flex-col">
-          <div className="flex flex-col items-center lg:items-start">
-            <h1 className="text-center text-[30px] md:text-[4rem] 2xl:text-[5rem] tracking-[10px] text-[#3C1D06] 2xl:mb-[1rem] font-bold">Experience!</h1>
-          </div>
-          <div className="flex items-center flex-col gap-[4rem] lg:flex-row 2xl:gap-[9rem] lg:justify-start mt-[3rem]">
-            <div id="cohereBox" className="flex items-center w-[1rem] h-[17rem] p-[30px] md:w-[17rem] md:h-[17rem] 2xl:w-[29rem] 2xl:h-[29rem] border-[3px] border-[#3C1D06]">
-              <div id="cohere" className="2xl:leading-[40px]">
-                <h2 className="text-[19px] 2xl:text-[33px] 2xl:leading-[50px] tracking-[1.5px] mb-[0.5rem] 2xl:mb-[1rem] font-semibold underline decoration-[12px] lg:decoration-[20px] underline-offset-[-8px] decoration-[#FACE5D]">Senior Data Quality Specialist</h2>
-                <h3 className="text-[16px] 2xl:text-[25px] 2xl:tracking-[1.5px] font-semibold">Cohere</h3>
-                <h4 className="text-[13px] 2xl:text-[20px] 2xl:tracking-[1.5px] mb-[1rem] 2xl:mb-[2rem]">September 2023 - Present</h4>
-                <p className="text-[12px] 2xl:text-[18px] 2xl:tracking-[0.5px] text-justify">Trains LLMs; Ranks and audits machine learning data by debugging, generating, and analyzing code snippets covering a wide variety of languages & libraries.</p>
+    <div>
+      <div className="h-auto px-[5rem] py-[10rem]">
+        <div className="flex justify-center">
+          <div className="flex flex-col">
+            <div className="flex flex-col items-center lg:items-start">
+              <h1 className="text-center text-[30px] md:text-[4rem] 2xl:text-[5rem] tracking-[10px] text-[#3C1D06] 2xl:mb-[1rem] font-bold">Experience!</h1>
+            </div>
+            <div className="flex items-center flex-col gap-[4rem] lg:flex-row 2xl:gap-[9rem] lg:justify-start mt-[3rem]">
+              <div id="cohereBox" className="flex items-center w-[17rem] h-[17rem] p-[30px] md:w-[17rem] md:h-[17rem] 2xl:w-[29rem] 2xl:h-[29rem] border-[3px] border-[#3C1D06]">
+                <div id="cohere" className="2xl:leading-[40px]">
+                  <h2 className="text-[19px] 2xl:text-[33px] 2xl:leading-[50px] tracking-[1.5px] mb-[0.5rem] 2xl:mb-[1rem] font-semibold underline decoration-[12px] lg:decoration-[20px] underline-offset-[-8px] decoration-[#FACE5D]">Senior Data Quality Specialist</h2>
+                  <h3 className="text-[16px] 2xl:text-[25px] 2xl:tracking-[1.5px] font-semibold">Cohere</h3>
+                  <h4 className="text-[13px] 2xl:text-[20px] 2xl:tracking-[1.5px] mb-[1rem] 2xl:mb-[2rem]">September 2023 - Present</h4>
+                  <p className="text-[12px] 2xl:text-[18px] 2xl:tracking-[0.5px] text-justify">Trains LLMs; Ranks and audits machine learning data by debugging, generating, and analyzing code snippets covering a wide variety of languages & libraries.</p>
+                </div>
               </div>
-            </div>
-            <div className="hidden lg:flex w-[17rem] h-[17rem] 2xl:w-[29rem] 2xl:h-[29rem] border-[3px] border-[#3C1D06] border-dashed">
-            </div>
-            <div className="hidden lg:flex w-[17rem] h-[17rem] 2xl:w-[29rem] 2xl:h-[29rem] border-[3px] border-[#3C1D06] border-dashed">
+              <div className="hidden lg:flex w-[17rem] h-[17rem] 2xl:w-[29rem] 2xl:h-[29rem] border-[3px] border-[#3C1D06] border-dashed">
+              </div>
+              <div className="hidden lg:flex w-[17rem] h-[17rem] 2xl:w-[29rem] 2xl:h-[29rem] border-[3px] border-[#3C1D06] border-dashed">
+              </div>
             </div>
           </div>
         </div>
       </div>
+      <div id="circles" className="translate-y-1/2 w-full">
+        <div className="flex items-center justify-center w-full overflow-x-auto whitespace-nowrap">
+          <div className="h-[80px] w-[80px] md:h-[100px] md:w-[100px] lg:h-[150px] lg:w-[150px] bg-[#EEB07E] rounded-full flex-shrink-0"></div>
+          <div className="h-[80px] w-[80px] md:h-[100px] md:w-[100px] lg:h-[150px] lg:w-[150px] bg-[#EEB07E] rounded-full flex-shrink-0"></div>
+          <div className="h-[80px] w-[80px] md:h-[100px] md:w-[100px] lg:h-[150px] lg:w-[150px] bg-[#EEB07E] rounded-full flex-shrink-0"></div>
+          <div className="h-[80px] w-[80px] md:h-[100px] md:w-[100px] lg:h-[150px] lg:w-[150px] bg-[#EEB07E] rounded-full flex-shrink-0"></div>
+          <div className="h-[80px] w-[80px] md:h-[100px] md:w-[100px] lg:h-[150px] lg:w-[150px] bg-[#EEB07E] rounded-full flex-shrink-0"></div>
+          <div className="h-[80px] w-[80px] md:h-[100px] md:w-[100px] lg:h-[150px] lg:w-[150px] bg-[#EEB07E] rounded-full flex-shrink-0"></div>
+          <div className="h-[80px] w-[80px] md:h-[100px] md:w-[100px] lg:h-[150px] lg:w-[150px] bg-[#EEB07E] rounded-full flex-shrink-0"></div>
+          <div className="h-[80px] w-[80px] md:h-[100px] md:w-[100px] lg:h-[150px] lg:w-[150px] bg-[#EEB07E] rounded-full flex-shrink-0"></div>
+          <div className="h-[80px] w-[80px] md:h-[100px] md:w-[100px] lg:h-[150px] lg:w-[150px] bg-[#EEB07E] rounded-full flex-shrink-0"></div>
+          <div className="h-[80px] w-[80px] md:h-[100px] md:w-[100px] lg:h-[150px] lg:w-[150px] bg-[#EEB07E] rounded-full flex-shrink-0"></div>
+          <div className="h-[80px] w-[80px] md:h-[100px] md:w-[100px] lg:h-[150px] lg:w-[150px] bg-[#EEB07E] rounded-full flex-shrink-0"></div>
+          <div className="h-[80px] w-[80px] md:h-[100px] md:w-[100px] lg:h-[150px] lg:w-[150px] bg-[#EEB07E] rounded-full flex-shrink-0"></div>
+          <div className="h-[80px] w-[80px] md:h-[100px] md:w-[100px] lg:h-[150px] lg:w-[150px] bg-[#EEB07E] rounded-full flex-shrink-0"></div>
+          <div className="h-[80px] w-[80px] md:h-[100px] md:w-[100px] lg:h-[150px] lg:w-[150px] bg-[#EEB07E] rounded-full flex-shrink-0"></div>
+          <div className="h-[80px] w-[80px] md:h-[100px] md:w-[100px] lg:h-[150px] lg:w-[150px] bg-[#EEB07E] rounded-full flex-shrink-0"></div>
+        </div>
+      </div>
+
     </div>
   );
 }
