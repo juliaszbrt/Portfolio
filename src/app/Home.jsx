79a938059c24f8be1e472cc00fb0393d354d19f5
juliaszbrt <112975933+juliaszbrt@users.noreply.github.com>
2024-10-22 23:42:22 -0400

    fix main page

--- src/app/Home.jsx
+++ src/app/Home.jsx
@@ -8,47 +8,18 @@ const murecho = Murecho({ subsets: ['latin'] })
 const Home = () => {
   return (
     <div className="h-screen bg-[#FFECDD]">
-      <div className="flex">
-        <div className="h-screen bg-[#FFDDC2] lg:flex lg:basis-1/3">
-          <div className="flex flex-row p-[3rem] invisible text-[#cb9e2e] absolute top-0 right-0 lg:visible lg:text-[20px] lg:gap-[3.5rem] 2xl:gap-[6rem] 2xl:text-[30px] ">
-            <Link href="./contact">
-                <button className="tracking-[3px]">
-                  About
-                </button>
-            </Link>
-            <Link href="./contact">
-                <button className="tracking-[3px]">
-                  Experience
-                </button>
-            </Link>
-            <Link href="./contact">
-                <button className="tracking-[3px]">
-                  Projects
-                </button>
-            </Link>
-            <Link href="./contact">
-                <button className="tracking-[3px]">
-                  Contact
-                </button>
-            </Link>
-          </div>
-        </div>
-        <div className="h-screen bg-[#FFECDD] lg:flex lg:basis-2/3">
-          <div className="absolute  lg:ml-[2.5rem] whitespace-nowrap top-1/2 transform -translate-y-1/2 left-1/2 -translate-x-1/2 lg:left-[62%]">
+        <div className="h-screen flex justify-center items-center">
               <motion.div
                   initial={{ opacity: 0 }}
                   whileInView={{ opacity: 1 }}
                   viewport={{
-                      amount: "all",
                       once: true,
                   }}
                   transition={{ duration: 1 }}>
-                <h1 className={`text-[#ffdf8f] text-center lg:text-left mt-[-2rem] text-[5rem] md:text-[11rem] 2xl:text-[16rem] font-black ${murecho.className}`}>ジュリア</h1>
-                <h2 className="text-[#f8c137] text-center lg:text-left mt-[-0.5rem] lg:mt-[-2rem] text-[2rem] md:text-[5rem] 2xl:text-[7rem] tracking-[5px] lg:tracking-[11px] font-bold">Julia Szubert</h2>
+                <h1 className={`text-[#ffdf8f] text-center mt-[-2rem] text-[5rem] md:text-[11rem] 2xl:text-[16rem] font-black ${murecho.className}`}>ジュリア</h1>
+                <h2 className="text-[#f8c137] text-center mt-[-0.5rem] text-[2rem] md:text-[5rem] 2xl:text-[7rem] tracking-[5px] lg:tracking-[11px] font-bold">Julia Szubert</h2>
               </motion.div>
-          </div>
         </div>
-      </div>
     </div>
   )
 }
