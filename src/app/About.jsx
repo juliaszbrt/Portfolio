052f5bc9953f21d4dced790fcae79fa652b77c7a
juliaszbrt <112975933+juliaszbrt@users.noreply.github.com>
2025-04-04 14:34:53 -0400

    fix formatting for small screens

--- src/app/About.jsx
+++ src/app/About.jsx
@@ -15,18 +15,18 @@ const About = () => {
             transition={{ duration: 3 }}
             className="lg:flex lg:flex-row lg:justify-center">
             <div className="lg:flex lg:gap-[5rem] lg:my-[4rem] lg:items-center lg:basis-3/4 lg:mx-[4rem] lg:justify-center 2xl:gap-[9rem]">
-                <div className="py-[2rem] flex flex-col justify-center gap-[2rem]">
-                    <div className="relative h-[18rem] w-[18rem] lg:h-[20rem] lg:w-[20rem] 2xl:h-[27rem] 2xl:w-[27rem] overflow-hidden">
+                <div className="py-[2rem] flex flex-col justify-center gap-[2rem] items-center">
+                    <div className="relative mt-[1.5rem] h-[17rem] w-[17rem] lg:mt-0 lg:h-[20rem] lg:w-[20rem] 2xl:h-[27rem] 2xl:w-[27rem] overflow-hidden">
                           <Image alt="profile photo" loading="lazy" width={1242} height={1242} src="/julia.png" ></Image>
                     </div>
-                    <p className="text-center leading-7 tracking-[1.8px]">
+                    <p className="text-[0.8rem] mx-[1.5rem] text-center leading-7 tracking-[1.8px]">
                           Bachelor of Science (BS) in Computer Science, CO-OP <br />
-                          Toronto Metropolitan University
+                          <b>Toronto Metropolitan University</b>
                     </p>
                 </div>
 
                 <div className="flex justify-self-center">
-                    <div className="flex flex-col justify-center mb-[3rem]">
+                    <div className="flex flex-col justify-center mb-[3rem] mx-[1.5rem]">
                         <h1 className="mb-[1rem] text-[1.7rem] tracking-[3px] font-semibold text-[#3C1D06] lg:text-[1.5rem] xl:text-[2.5rem]">
                             Nice to meet you!
                         </h1>
